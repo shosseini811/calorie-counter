2b17371f4900a8a54c29bfe5562bcc90057fedee
shosseini811 <s.sadathosseini@gmail.com>
2025-05-26 20:29:41 -0400

    Final changes to Electron branch before merging to main

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// This line makes sure that when you build the final version of your app for Windows users, it doesn't show an unnecessary command window.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// This imports the Manager trait from the tauri library.
 use tauri::Manager;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+// - tauri::command specifically tells Tauri that the following Rust function ( greet or get_backend_url in this case) should be callable from your JavaScript frontend.
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -13,7 +17,8 @@ fn greet(name: &str) -> String {
 fn get_backend_url() -> String {
     "http://localhost:5001".to_string()
 }
-
+// - tauri::mobile_entry_point tells Tauri (the framework you're using to build the app) that this run function is the starting point for the mobile app.
+// If this is a mobile app, mark this function as the main starting place for mobile.
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
